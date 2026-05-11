f7f4d3508303b3b6af6a7c983fe020cac0686413
josh.apfel <joshua.l.apfel@gmail.com>
2026-05-11 19:29:29 -0400

    updated source code for importing to fastapi app

--- code/src/main.py
+++ code/src/main.py
@@ -1,14 +1,18 @@
 from feedpoller import FeedPoller 
 import json
 
-REUTERS_URL = """
-https://news.google.com/rss/search?q=site%3A%20reuters.com&hl=en-US&gl=US&ceid=US%3Aen
-""" 
+REUTERS_URL = ("https://news.google.com/rss/search"
+"q=site%3A%20reuters.com&hl=en-US&gl=US&ceid=US%3Aen"
+)
 
-if __name__ == "__main__":
-  poller = FeedPoller(REUTERS_URL)
+def main(url: str = REUTERS_URL) -> dict[str, bool]:
+  poller = FeedPoller(url)
   changed = poller.poll()
   if changed:
       print("Feed changed — new file saved")
-  else:
-      print("No change")
+      return {"Feed changed": True}
+  print("No change")
+  return {"Feed changed": False}
+
+if __name__ == "__main__":
+    main()
