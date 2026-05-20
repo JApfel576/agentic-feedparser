7d14ea6f1136505c5bd9a6ed7d790dba9ee0280e
josh.apfel <joshua.l.apfel@gmail.com>
2026-05-20 19:48:38 -0400

    new module for feed agents

---
+++ packages/feed_agents/src/feed_agents/feed_agents.py
@@ -0,0 +1,22 @@
+from dotenv import load_dotenv
+import os
+from langchain.agents import create_agent
+
+load_dotenv()
+api_key = os.getenv("OPENAI_API_KEY")
+
+def guess_url(site: str) -> str:
+  """Guess the url"""
+  return f"The url would be {site}"
+
+agent = create_agent(
+  model = "openai:gpt-4o-mini",
+  tools = [guess_url],
+  system_prompt="You are a helpful assistant"
+)
+
+result = agent.invoke(
+  {"messages":[{"role":"user", "content":"What is the full url after appending site: example.com to query in news.google.com/search"}]}
+)
+
+print(result["messages"][-1].content_blocks)
