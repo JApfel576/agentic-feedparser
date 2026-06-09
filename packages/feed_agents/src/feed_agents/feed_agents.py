3b4ce8362ec1cdca1df7fa0d090d98cc31fd895c
josh.apfel <joshua.l.apfel@gmail.com>
2026-06-09 14:38:21 -0400

    working on creating multi agent workflow

--- packages/feed_agents/src/feed_agents/feed_agents.py
+++ packages/feed_agents/src/feed_agents/feed_agents.py
@@ -15,6 +15,7 @@ from pydantic import BaseModel, Field
 from langchain_core.output_parsers import PydanticOutputParser
 from langchain_core.prompts import PromptTemplate
 from langgraph.types import Command
+from langgraph.prebuilt import ToolNode
 
 
 load_dotenv()
@@ -25,16 +26,6 @@ SERPER_API_KEY = os.getenv("SERPER_API_KEY")
 SYSTEM_PROMPT = """You are an agent - please keep going until the user's query is completely resolved, before ending your turn and yielding back to the user. Only terminate your turn when you are sure that the problem is solved. You MUST plan extensively before each function call, and reflect extensively on the outcomes of the previous function calls. DO NOT do this entire process by making function calls only, as this can impair your ability to solve the problem and think insightfully."""
 
 
-class MessagesState(TypedDict):
-    messages: Annotated[list[AnyMessage], operator.add]
-    llm_calls: int
-
-
-class RoutingState(TypedDict):
-    messages: list[AnyMessage]
-    tool_type: str
-
-
 class SiteResponseOutput(BaseModel):
     """Structure an output for sites suggested by LLM for input topic"""
 
@@ -43,6 +34,7 @@ class SiteResponseOutput(BaseModel):
 
 
 search = GoogleSerperAPIWrapper()
+
 
 @tool(args_schema=SiteResponseOutput)
 def relevant_site(topic: str, sites: list[str]) -> list[str]:
@@ -64,96 +56,160 @@ def guess_url(topic: str, sites: list[str]) -> list[str]:
     return full_urls
 
 
+class MessagesState(TypedDict):
+    messages: Annotated[list[AnyMessage], operator.add]
+    llm_calls: int
+
+
+class SearchAgent():
+    def __init__(self, model_name: str = "gpt-4o-mini", tools: list = [relevant_site]):
+        self.llm = ChatOpenAI(model = model_name, temperature=0)
+        self.llm_with_tools = self.llm.bind_tools(tools)
+        self.system_prompt = """You are an agent - please keep going until the user's query is completely resolved, before ending your turn and yielding back to the user. Only terminate your turn when you are sure that the problem is solved. You MUST plan extensively before each function call, and reflect extensively on the outcomes of the previous function calls. DO NOT do this entire process by making function calls only, as this can impair your ability to solve the problem and think insightfully."""
+
+    def run(self, state: MessagesState) -> dict:
+        messages = [SystemMessage(content=self.system_prompt)] + state["messages"]
+        response = self.llm_with_tools.invoke(messages)
+        return {"messages": state["messages"] + [response]}
+
+
+def _router(state:MessagesState) -> str:
+    return state.get("next_step", "finish")
+
+def finish_node(state:MessagesState) -> None:
+    pass
+
 tools = [relevant_site, guess_url]
-tools_by_name = {tool.name: tool for tool in tools}
-model = ChatOpenAI(model = "gpt-4o-mini", temperature=0)
-model_with_tools = model.bind_tools(tools)
 
+search_agent = SearchAgent()
+builder = StateGraph(MessagesState)
+builder.add_node("search_node", search_agent.run)
+builder.add_node("tools_node", ToolNode(tools=tools))
+# builder.add_node("tools_node", finish_node)
 
-def llm_call(state: dict):
-    """LLM decides whether to call a tool or not"""
-    return {
-        "messages": [
-            model_with_tools.invoke(
-                [SystemMessage(content=SYSTEM_PROMPT)] + state["messages"]
-            )
-        ],
-        "llm_calls": state.get("llm_calls", 0) + 1,
-    }
+builder.add_edge(START, "search_node")
+builder.add_edge("tools_node", "search_node")
+builder.add_edge("search_node", END)
+# builder.add_edge("finish_node", END)
 
-
-def tool_node(state: dict):
-    """Performs the tool call"""
-    result = []
-    for tool_call in state["messages"][-1].tool_calls:
-        tool = tools_by_name[tool_call["name"]]
-        observation = tool.invoke(tool_call["args"])
-        result.append(ToolMessage(content=observation, tool_call_id=tool_call["id"]))
-    return {"messages": result}
-
-
-def should_continue(state: MessagesState) -> Literal["tool_node", END]:
-    """Decide if we should continue the loop or not based on if LLM made tool call"""
-
-    messages = state["messages"]
-    last_message = messages[-1]
-
-    # Intercept the specific tool response and force an exit
-    if hasattr(last_message, "name") and last_message.name == "guess_url":
-        return END
-
-    """If the LLM performs a tool call then perform an action"""
-    if last_message.tool_calls:
-        return "tool_node"
-    """Otherwise we stop, (reply to the user)"""
-    return END
-
-
-# Build workflow
-agent_builder = StateGraph(MessagesState)
-
-# Add nodes
-agent_builder.add_node("llm_call", llm_call)
-agent_builder.add_node("tool_node", tool_node)
-
-# Add edges to connect nodes
-agent_builder.add_edge(START, "llm_call")
-agent_builder.add_conditional_edges("llm_call", should_continue, {"tool_node": "tool_node", END: END})
-
-
-# Compile agent
 checkpointer = MemorySaver()
-agent = agent_builder.compile(checkpointer=checkpointer)
-
-# Show agent
-display(Image(agent.get_graph(xray=True).draw_mermaid_png()))
-
+app = builder.compile(checkpointer = checkpointer)
 # Topic for message
 topic = "latest reliable news source affecting stock market"
-
-# # Invoke with sequential messages instead of sending them all at once
-config = {"configurable": {"thread_id": "1"}}
-
-# # List for agent invocation input
-agent_invoke_list = [
+messages = app.invoke(
     {
-        "message_content": f"What is a reliable site for {topic} through TLD as .com? What is the full URL after appending it as search query to Google News URL? Generate the final output as a JSON.",
-        "config": config,
+        "messages":[HumanMessage(content=f"What is a reliable site for {topic} through TLD as .com?")]
     },
-]
-
-
-def agent_invoke_message(item: list[dict]) -> MessagesState:
-    """Invoke agent to execute messages synchronously"""
-    messages = agent.invoke(
-        {"messages": [HumanMessage(content=item["message_content"])]},
-        item["config"],
-    )
-    return messages
-
-
-for item in agent_invoke_list:
-    messages = agent_invoke_message(item)
+    {"configurable": {"thread_id": "1"}}
+     )
 
 for m in messages["messages"]:
     m.pretty_print()
+
+
+
+# # # Invoke with sequential messages instead of sending them all at once
+# config = {"configurable": {"thread_id": "1"}}
+
+# # # List for agent invocation input
+# agent_invoke_list = [
+#     {
+#         "message_content": f"What is a reliable site for {topic} through TLD as .com? What is the full URL after appending it as search query to Google News URL? Generate the final output as a JSON.",
+#         "config": config,
+#     },
+# ]
+
+
+# tools = [relevant_site, guess_url]
+# tools_by_name = {tool.name: tool for tool in tools}
+# model = ChatOpenAI(model = "gpt-4o-mini", temperature=0)
+# model_with_tools = model.bind_tools(tools)
+
+
+# def llm_call(state: dict):
+#     """LLM decides whether to call a tool or not"""
+#     return {
+#         "messages": [
+#             model_with_tools.invoke(
+#                 [SystemMessage(content=SYSTEM_PROMPT)] + state["messages"]
+#             )
+#         ],
+#         "llm_calls": state.get("llm_calls", 0) + 1,
+#     }
+
+
+# def tool_node(state: dict):
+#     """Performs the tool call"""
+#     result = []
+#     for tool_call in state["messages"][-1].tool_calls:
+#         tool = tools_by_name[tool_call["name"]]
+#         observation = tool.invoke(tool_call["args"])
+#         result.append(ToolMessage(content=observation, tool_call_id=tool_call["id"]))
+#     return {"messages": result}
+
+
+# def should_continue(state: MessagesState) -> Literal["tool_node", END]:
+#     """Decide if we should continue the loop or not based on if LLM made tool call"""
+
+#     messages = state["messages"]
+#     last_message = messages[-1]
+
+#     # Intercept the specific tool response and force an exit
+#     if hasattr(last_message, "name") and last_message.name == "guess_url":
+#         return END
+
+#     """If the LLM performs a tool call then perform an action"""
+#     if last_message.tool_calls:
+#         return "tool_node"
+#     """Otherwise we stop, (reply to the user)"""
+#     return END
+
+
+# # Build workflow
+# agent_builder = StateGraph(MessagesState)
+
+# # Add nodes
+# agent_builder.add_node("llm_call", llm_call)
+# agent_builder.add_node("tool_node", tool_node)
+
+# # Add edges to connect nodes
+# agent_builder.add_edge(START, "llm_call")
+# agent_builder.add_conditional_edges("llm_call", should_continue, {"tool_node": "tool_node", END: END})
+
+
+# # Compile agent
+# checkpointer = MemorySaver()
+# agent = agent_builder.compile(checkpointer=checkpointer)
+
+# # Show agent
+# display(Image(agent.get_graph(xray=True).draw_mermaid_png()))
+
+# # Topic for message
+# topic = "latest reliable news source affecting stock market"
+
+# # # Invoke with sequential messages instead of sending them all at once
+# config = {"configurable": {"thread_id": "1"}}
+
+# # # List for agent invocation input
+# agent_invoke_list = [
+#     {
+#         "message_content": f"What is a reliable site for {topic} through TLD as .com? What is the full URL after appending it as search query to Google News URL? Generate the final output as a JSON.",
+#         "config": config,
+#     },
+# ]
+
+
+# def agent_invoke_message(item: list[dict]) -> MessagesState:
+#     """Invoke agent to execute messages synchronously"""
+#     messages = agent.invoke(
+#         {"messages": [HumanMessage(content=item["message_content"])]},
+#         item["config"],
+#     )
+#     return messages
+
+
+# for item in agent_invoke_list:
+#     messages = agent_invoke_message(item)
+
+# for m in messages["messages"]:
+#     m.pretty_print()
