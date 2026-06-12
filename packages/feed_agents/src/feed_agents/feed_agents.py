4fb1f80bce873603f5c3e88502e35e26697726de
josh.apfel <joshua.l.apfel@gmail.com>
2026-06-12 12:41:11 -0400

    moving tools node to agent class with subgraph

--- packages/feed_agents/src/feed_agents/feed_agents.py
+++ packages/feed_agents/src/feed_agents/feed_agents.py
@@ -5,7 +5,7 @@ from langchain.tools import tool
 from langchain.chat_models import init_chat_model, BaseChatModel
 from langgraph.graph import StateGraph, START, END
 from langgraph.checkpoint.memory import MemorySaver
-from langchain.messages import AnyMessage, SystemMessage, HumanMessage
+from langchain.messages import AnyMessage, SystemMessage, HumanMessage, ToolMessage
 from typing_extensions import TypedDict
 from typing import Annotated, Literal, Callable
 import operator
@@ -57,6 +57,30 @@ def guess_url(topic: str, sites: list[str]) -> dict:
 
 
 tools = [relevant_site, guess_url]
+# tools_by_name = {tool.name: tool for tool in tools}
+
+
+# def tool_node(state: dict):
+#     """Performs the tool call"""
+#     result = []
+#     for tool_call in state["messages"][-1].tool_calls:
+#         tool = tools_by_name[tool_call["name"]]
+#         observation = tool.invoke(tool_call["args"])
+#         result.append(ToolMessage(content=observation, tool_call_id=tool_call["id"]))
+#     return {"messages": result}
+
+
+# def should_continue(state: MessagesState) -> Literal["tool_node", "__end__"]:
+#     """Decide if we should continue the loop or not based on if LLM made tool call"""
+
+#     messages = state["messages"]
+#     last_message = messages[-1]
+
+#     """If the LLM performs a tool call then perform an action"""
+#     if last_message.tool_calls:
+#         return "tools"
+#     """Otherwise we route back to agent"""
+#     return END
 
 
 # Agent State
@@ -65,19 +89,32 @@ class MessagesState(TypedDict):
 
 
 class DefaultAgent():
-    def __init__(self, model: str, tools: list):
-        self.llm = init_chat_model(model=model, temperature=0)
-        self.llm_with_tools = self.llm.bind_tools(tools=tools)
-        self.system_prompt = """You are an agent - please keep going until the user's query is completely resolved, before ending your turn and yielding back to the user. Only terminate your turn when you are sure that the problem is solved. You MUST plan extensively before each function call, and reflect extensively on the outcomes of the previous function calls. DO NOT do this entire process by making function calls only, as this can impair your ability to solve the problem and think insightfully."""
-
-    def run(self, state: MessagesState) -> dict:
-        return {
+    def __init__(self, state: MessagesState, model: str, tools: list):
+        def call_agent(self, state: MessagesState) -> dict:
+            return {
             "messages": [
                 self.llm_with_tools.invoke(
-                    [SystemMessage(content=self.system_prompt)] + state["messages"]
+                    [SystemMessage(content=self.system_prompt)] + state["messages"],
+                    config = {"configurable": {"thread_id": "1"}}
                 )
             ]
         }
+        workflow = StateGraph(MessagesState)
+
+        workflow.add_node("agent", call_agent)
+        workflow.add_node("tools", ToolNode(tools))
+
+        workflow.add_edge(START, "agent")
+        workflow.add_conditional_edges("agent", tools_condition, {"tools": "tools", END: END})
+        workflow.add_edge("tools", "agent")
+
+        checkpointer = MemorySaver()
+        self.graph = workflow.compile(checkpointer=checkpointer)
+
+        self.llm = init_chat_model(model=model, temperature=0)
+        self.llm_with_tools = self.llm.bind_tools(tools=tools)
+        self.system_prompt = """You are an agent - please keep going until the user's query is completely resolved, before ending your turn and yielding back to the user. Only terminate your turn when you are sure that the problem is solved. You MUST plan extensively before each function call, and reflect extensively on the outcomes of the previous function calls. DO NOT do this entire process by making function calls only, as this can impair your ability to solve the problem and think insightfully."""
+        return state
     
 def make_supervisor_node(llm: BaseChatModel, members: list[str]) -> Callable[[MessagesState], Command[str]]:
     options = ["FINISH"] + members
@@ -94,7 +131,7 @@ def make_supervisor_node(llm: BaseChatModel, members: list[str]) -> Callable[[Me
         messages = [
             {"role":"system", "content": system_prompt}
         ] + state["messages"]
-        response = llm.with_structured_output(Router).invoke(messages)
+        response = llm.with_structured_output(Router).invoke(messages, config={"configurable": {"thread_id": "1"}})
         choice = response["next"]
         goto = END if choice == "FINISH" else choice
         return Command(goto=goto, update={"next":choice})
@@ -102,26 +139,31 @@ def make_supervisor_node(llm: BaseChatModel, members: list[str]) -> Callable[[Me
 
 # TO DO update to return search node agent to supervisor 
 def search_node(state: MessagesState):
-    search_agent = DefaultAgent(model = model, tools=tools)
-    search_agent.run
+    search_agent = DefaultAgent(state = state, model = model, tools=tools)
+    result = search_agent
+    return Command(
+        update={
+            "messages":[
+                HumanMessage(content=result["messages"][-1],name="search")
+            ]
+        },
+        goto = "supervisor",
+    )
 
 
 model = "openai:gpt-4o-mini"
 search_supervisor_node = make_supervisor_node(
-    init_chat_model(model), ["search_node"])
+    init_chat_model(model), ["search"])
 
 
 builder = StateGraph(MessagesState)
-builder.add_node("search_supervisor_node", search_supervisor_node)
-builder.add_node("search_node", search_node)
-builder.add_node("tools_node", ToolNode(tools=tools))
+builder.add_node("supervisor", search_supervisor_node)
+builder.add_node("search", search_node)
 
-builder.add_edge(START, "search_supervisor_node")
-builder.add_edge("search_supervisor_node", "search_node")
-builder.add_conditional_edges(
-    "search_node", tools_condition, {"tools": "tools_node", END: END}
-)
-# builder.add_edge("tools_node", "search_supervisor_node")
+
+builder.add_edge(START, "supervisor")
+builder.add_edge("supervisor", "search")
+builder.add_edge("search", "supervisor")
 
 
 checkpointer = MemorySaver()
@@ -140,15 +182,15 @@ messages = app.invoke(
     {"configurable": {"thread_id": "1"}},
 )
 
-# for m in messages["messages"]:
-#     m.pretty_print()
+for m in messages["messages"]:
+    m.pretty_print()
 
-for s in app.stream({
-        "messages": [
-            HumanMessage(
-                content=f"What is a reliable site for {topic} through TLD as .com? What is the full URL after appending it as search query to Google News URL?"
-            )
-        ]
-    },
-    config={"configurable": {"thread_id": "1"}}):
-    print(s)
+# for s in app.stream({
+#         "messages": [
+#             HumanMessage(
+#                 content=f"What is a reliable site for {topic} through TLD as .com? What is the full URL after appending it as search query to Google News URL?"
+#             )
+#         ]
+#     },
+#     config={"configurable": {"thread_id": "1"}}):
+#     print(s)
